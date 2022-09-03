211fe2560f3266fbc91b378f992a9b0180c19576
pavelforever <pavel_brov@mail.ru>
2022-09-03 14:45:35 +0300

    newer version because with old i got errors. + show log live

--- lxd-provisioning/bootstrap-kube.sh
+++ lxd-provisioning/bootstrap-kube.sh
@@ -4,22 +4,22 @@
 # For other versions of Ubuntu, you might need some tweaking
 
 echo "[TASK 0] Install essential packages"
-apt install -qq -y net-tools curl ssh software-properties-common >/dev/null 2>&1
+apt install -qq -y net-tools curl ssh software-properties-common #>/dev/null 2>&1
 
 echo "[TASK 1] Install containerd runtime"
-apt update -qq >/dev/null 2>&1
-apt install -qq -y containerd apt-transport-https >/dev/null 2>&1
+apt update -qq #>/dev/null 2>&1
+apt install -qq -y containerd apt-transport-https #>/dev/null 2>&1
 mkdir /etc/containerd
 containerd config default > /etc/containerd/config.toml
 systemctl restart containerd
-systemctl enable containerd >/dev/null 2>&1
+systemctl enable containerd #>/dev/null 2>&1
 
 echo "[TASK 2] Add apt repo for kubernetes"
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - #>/dev/null 2>&1
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" #>/dev/null 2>&1
 
 echo "[TASK 3] Install Kubernetes components (kubeadm, kubelet and kubectl)"
-apt install -qq -y kubeadm=1.22.0-00 kubelet=1.22.0-00 kubectl=1.22.0-00 >/dev/null 2>&1
+apt install -qq -y kubeadm kubelet kubectl #>/dev/null 2>&1
 echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/default/kubelet
 systemctl restart kubelet
 
@@ -43,20 +43,20 @@ if [[ $(hostname) =~ .*master.* ]]
 then
 
   echo "[TASK 7] Pull required containers"
-  kubeadm config images pull >/dev/null 2>&1
+  kubeadm config images pull #>/dev/null 2>&1
 
   echo "[TASK 8] Initialize Kubernetes Cluster"
-  kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all >> /root/kubeinit.log 2>&1
+  kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all #>> /root/kubeinit.log 2>&1
 
   echo "[TASK 9] Copy kube admin config to root user .kube directory"
   mkdir /root/.kube
-  cp /etc/kubernetes/admin.conf /root/.kube/config  
+  cp /etc/kubernetes/admin.conf /root/.kube/config
 
   echo "[TASK 10] Deploy Flannel network"
-  kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml > /dev/null 2>&1
+  kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml #> /dev/null 2>&1
 
   echo "[TASK 11] Generate and save cluster join command to /joincluster.sh"
-  joinCommand=$(kubeadm token create --print-join-command 2>/dev/null) 
+  joinCommand=$(kubeadm token create --print-join-command 2>/dev/null)
   echo "$joinCommand --ignore-preflight-errors=all" > /joincluster.sh
 
 fi
